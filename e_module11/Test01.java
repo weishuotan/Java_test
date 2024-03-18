2fb5ccceb9d7ad215df8d06a3ece7ab3cbc574a2
happy_tan <120439538+weishuotan@users.noreply.github.com>
2024-03-18 22:28:11 +0800

    Add files via upload

---
+++ e_module11/Test01.java
@@ -0,0 +1,8 @@
+package com.atguigu.e_module11;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(USB.NUM1);
+        System.out.println(USB.NUM2);
+    }
+}
---
+++ e_module11/USB.java
@@ -0,0 +1,6 @@
+package com.atguigu.e_module11;
+
+public interface USB {
+    public static final int NUM1 = 100;
+    int NUM2 = 200;
+}
